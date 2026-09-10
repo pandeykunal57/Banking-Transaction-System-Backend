ab15d01926d2cd7dbd6cf0edab1857d6424fd9c3
pandeykunal57 <148645697+pandeykunal57@users.noreply.github.com>
2026-09-10 13:41:18 +0530

    MONGODB Connected

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
 const app = require('./src/app');
+const connectToDB = require('./config/db');
 
+connectToDB()
+ 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 })
