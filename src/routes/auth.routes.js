33c4d57b1ed4b0d700b46aed3a83c86ea01860ef
pandeykunal57 <148645697+pandeykunal57@users.noreply.github.com>
2026-09-11 13:40:19 +0530

    Login Route, Controller Added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,5 +8,8 @@ const router = express.Router()
 router.post("/register", authController.userRegisterController)
 
 
+/* POST /api/auth/login */
+router.post("/login",authController.userLoginController)
+
 
 module.exports = router
