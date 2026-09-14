5ed5b320a729c321b0a0d30b2d889b33ead889dc
pandeykunal57 <148645697+pandeykunal57@users.noreply.github.com>
2026-09-14 18:51:27 +0530

    Account Route, Controller Updated

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -17,8 +17,19 @@ async function createAccountController(req, res) {
 }
 
 
+async function getUserAccountsController(req, res) {
+
+    const accounts = await accountModel.find({ user: req.user._id });
+
+    res.status(200).json({
+        accounts
+    })
+}
+
+
 
 
 module.exports = {
-    createAccountController
+    createAccountController,
+    getUserAccountsController
 }
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -18,6 +18,13 @@ const router = express.Router()
 router.post("/", authMiddleware.authMiddleware, accountController.createAccountController)
 
 
+/**
+ * - GET /api/accounts/
+ * - Get all accounts of the logged-in user
+ * - Protected Route
+ */
+router.get("/", authMiddleware.authMiddleware, accountController.getUserAccountsController)
+
 
 
 
